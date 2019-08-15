3b4e433386963ab80bd92557a7e908798c34c0c8
Sun <1677724535@qq.com>
2019-08-15 11:42:51 +0800

    t

---
+++ src/main/java/com/hcjava/dao/UserDao.java
@@ -0,0 +1,5 @@
+package com.hcjava.dao;
+
+public interface UserDao {
+
+}
---
+++ src/main/java/com/hcjava/entity/Result.java
@@ -0,0 +1,39 @@
+package com.hcjava.entity;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import lombok.Data;
+
+@Data
+public class Result<T> {
+	private int code;
+	private String msg;
+	private T data;
+	
+	public Result() {
+		this.code=0;
+		this.msg="ok";
+	}
+	
+	public Result(String msg) {
+		this.code=0;
+		this.msg=msg;
+	}
+	
+	public Result(T data) {
+		this.code=0;
+		this.msg="ok";
+		this.data=data;
+	}
+	
+	public static <T> Result<T> newSuccess() {
+		return new Result<>();
+	}
+	
+	public static <T> Result<T> newSuccess(T obj) {
+		Result<T> result = new Result<>();
+		result.setData(obj);
+		return result;
+	}
+
+}
